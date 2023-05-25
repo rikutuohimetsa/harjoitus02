96d524844dd9366f21699f260630398363b7b331
Riku Tuohimetsä <riku.tuohimetsa@adafy.com>
2023-05-25 09:25:19 +0300

    switched languages and added error handling to get call

--- t4/skriptit.js
+++ t4/skriptit.js
@@ -1,24 +1,28 @@
-function getAlbumit() {
-    $.get("https://itunes.apple.com/lookup/?id=1472380750&entity=album", function(data, status){
-        const albumit = JSON.parse(data);
-        console.log(albumit);
+function getAlbums() {
+    let url = "https://itunes.apple.com/lookup/?id=1472380750&entity=album";
+    $.get(url, function(data, status){
+        if (status != "success") {
+            console.log(`Error getting ${url}`);
+            return;
+        }
+        const albums = JSON.parse(data);
         
-        for (var key in albumit){
-            for (var key1 in albumit[key]) {
-                luoHTML(albumit[key][key1]);
+        for (var key in albums){
+            for (var key1 in albums[key]) {
+                addCard(albums[key][key1]);
             }
         }
     });
 
 }
 
-function luoHTML(albumi) {
-    if (albumi.collectionType != "Album") return;
+function addCard(album) {
+    if (album.collectionType != "Album") return;
     let card = "<div class=\"col-12 col-lg-3 \">" +
                "<div class=\"card h-100\" >" +
-                   `<img class=\"card-img-top\" src=\"${albumi.artworkUrl100.replace("100x100bb", "1024x1024bb")}\" alt=\"${albumi.collectionName}\">` +
+                   `<img class=\"card-img-top\" src=\"${album.artworkUrl100.replace("100x100bb", "1024x1024bb")}\" alt=\"${album.collectionName}\">` +
                    "<div class=\"card-body\">" +
-                       `<h5 class=\"card-title\">${albumi.collectionName}</h5>` +
+                       `<h5 class=\"card-title\">${album.collectionName}</h5>` +
                    "</div>" +
                "</div>"+ 
            "</div> ";
